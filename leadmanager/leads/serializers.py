e5fe3875179ac0ed280463e52389b7d6aabd717e
Fernando Navarro <fernando@coreddd.com>
2020-10-04 21:26:25 -0700

    React-Django-TM

---
+++ leadmanager/leads/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from leads.models import Lead
+
+# Lead Serializer
+
+
+class LeadSerializer(serializers.ModelSerializer):
+    """
+    docstring
+    """
+    class Meta:
+        model = Lead
+        fields = '__all__'
